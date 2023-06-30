0ebf0b48c62ca29eb4cebdfea8a255cb5bc975fb
devgenx <sebastian110420@gmail.com>
2023-06-30 12:33:32 -0700

    fixed light mode errors on text colors for users and tutorials section

--- components/Users.tsx
+++ components/Users.tsx
@@ -15,7 +15,7 @@ const Users = () => {
     <div className="container mx-auto min-h-screen">
       <div className="flex flex-col justify-center items-center">
         <Heading size="lg">Meet and Add Friends</Heading>
-        <Paragraph className="m-5 font-extralight">
+        <Paragraph className="m-5 font-extralight text-black dark:text-white">
           Start chatting anonymously with users by adding them as friends
         </Paragraph>
         <hr className="w-1/2 h-0.5 mx-auto mt-3 bg-neutral-400 border-0 dark:bg-neutral-200 "></hr>
